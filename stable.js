0a5e9e6ae4d3850741299670b3c75ed2cf147dc0
shiro <181418187+wendeshiro@users.noreply.github.com>
2024-09-19 17:42:53 -0700

    added a stable and a horse

---
+++ stable.js
@@ -0,0 +1,18 @@
+console.log("horse22223!");
+
+let horseName = "shiba"; // this is a comment
+
+horseName = "shiba2";
+let horseAge = 18;
+console.log(horseName);
+console.log(horseAge);
+
+let isHorseInStable = true;
+console.log(isHorseInStable);
+
+isHorseInStable = false;
+console.log(isHorseInStable)
+
+horseName = "shiba3";
+console.log(horseName);
+
