f7ad4747c36e10fb8ee3241ec7b3bbcf28a07f45
shiro <181418187+wendeshiro@users.noreply.github.com>
2024-09-26 14:37:46 -0700

    add operators to my stable

--- stable.js
+++ stable.js
@@ -16,3 +16,34 @@ console.log(isHorseInStable)
 horseName = "shiba3";
 console.log(horseName);
 
+console.log(typeof "52321412313");
+
+console.log(typeof 52321412313);
+
+console.log(typeof isHorseInStable);
+
+console.log(typeof horseAge);
+
+const STABLE_MONTHLY_FEE = 100;
+console.log(STABLE_MONTHLY_FEE);
+console.log(2 + 3);
+console.log(STABLE_MONTHLY_FEE - 2);
+console.log(2 * 3);
+console.log(STABLE_MONTHLY_FEE / 2);
+console.log(STABLE_MONTHLY_FEE ** 2);
+
+let horseAgeIn10Years = horseAge + 10;
+let horseAgeIn20Years = horseAge + 20;
+console.log(horseAgeIn10Years);
+console.log(horseAgeIn20Years);
+
+console.log(horseName + " is the name of my horse")
+
+let horseIntroduction = " is the name of my horse. test 2"
+console.log(horseName + horseIntroduction)
+
+let rentIntro = "It costs " + STABLE_MONTHLY_FEE + " monthly"
+console.log(rentIntro)
+
+let rentIntro2 = `It costs ${STABLE_MONTHLY_FEE} monthly. test 2`
+console.log(rentIntro2)
